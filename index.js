a5a5406603aa89f880073af19d0cdabf0d10d6bc
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-05-14 00:27:33 +0300

    feat: make it compatible with mos@1
    BREAKING CHANGE: can not work with mos@0

--- index.js
+++ index.js
@@ -1,58 +1,6 @@
 'use strict'
-module.exports = plugin
+module.exports = (mos, md) => Object.assign(mos.scope, require('./lib')(md))
 
-const renderDeps = require('./lib/render-deps')
-const shielder = require('shields')
-
-function plugin (markdown) {
-  return {
-    dependencies: opts => {
-      opts = opts || {}
-
-      return `## <a name="dependencies">Dependencies</a>${maybeShield('deps', opts.shield)}\n\n${
-        renderDeps({
-          deps: markdown.pkg.dependencies,
-          pkgRoot: markdown.pkgRoot,
-        })
-      }\n`
-    },
-    devDependencies: opts => {
-      opts = opts || {}
-
-      return `## <a name="dev-dependencies">Dev Dependencies</a>${maybeShield('devDeps', opts.shield)}\n\n${
-        renderDeps({
-          deps: markdown.pkg.devDependencies,
-          pkgRoot: markdown.pkgRoot,
-        })
-      }\n`
-    },
-  }
-
-  function maybeShield (shieldName, style) {
-    if (!~['boolean', 'string'].indexOf(typeof style) || !style) {
-      return ''
-    }
-
-    style = style === true ? 'flat' : style
-
-    testRepo(markdown.repo)
-
-    const getShieldProps = shielder({style})
-    const shieldProps = getShieldProps(shieldName, {
-      repo: `${markdown.repo.user}/${markdown.repo.repo}`,
-      branch: 'master',
-    })
-
-    return ` ${renderShield(shieldProps)}`
-  }
-}
-
-function testRepo (repo) {
-  if (!repo || repo.host !== 'github.com') {
-    throw new Error('The shields plugin only works for github repos')
-  }
-}
-
-function renderShield (shieldProps) {
-  return `[![${shieldProps.text}](${shieldProps.image})](${shieldProps.link})`
+module.exports.attributes = {
+  pkg: require('./package.json'),
 }
